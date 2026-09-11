c8cad947e1506168b66e14b30759c4b6cb880008
Mert <114917894+MertDilbaz@users.noreply.github.com>
2026-09-11 11:45:20 +0300

    feat(hero): add theme-aware Mevzun hub

---
+++ resources/views/components/hero-mevzun-hub.blade.php
@@ -0,0 +1,19 @@
+<div {{ $attributes->class(['hero-mevzun-hub rounded-[6px] border border-[var(--border)] bg-[var(--bg-elevated)] px-5 py-4 text-center']) }}>
+    <div class="mx-auto flex h-12 w-12 items-center justify-center" aria-hidden="true">
+        <img
+            src="/images/brand/mevzun-icon-light-128.png"
+            alt=""
+            width="48"
+            height="48"
+            class="block h-12 w-12 object-contain dark:hidden"
+        />
+        <img
+            src="/images/brand/mevzun-icon-dark-128.png"
+            alt=""
+            width="48"
+            height="48"
+            class="hidden h-12 w-12 object-contain dark:block"
+        />
+    </div>
+    <div class="mt-2 text-[17px] font-semibold tracking-[-0.01em] text-[var(--text-primary)]">Mevzun</div>
+</div>
